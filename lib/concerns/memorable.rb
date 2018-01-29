655dd1d641dda4746cbc2fc6cc7c3116167e015c
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-29 07:30:42 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,19 @@
 module Memorable
-  def reset_all
-    all.clear
+
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
+
+    def count
+      all.count
+    end
   end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+
 end
