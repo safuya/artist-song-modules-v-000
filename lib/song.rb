373ead620050c063e73a8db5dd77570fc7a1ed87
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-29 07:30:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 class Song
+  extend Findable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
